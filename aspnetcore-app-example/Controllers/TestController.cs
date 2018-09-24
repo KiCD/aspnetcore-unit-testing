d10604e988d41850f58c441830d5f7ac8d8555ce
Cristina Dumitrascu <cristina.dumitrascu@steadforce.com>
2018-09-24 10:37:13 +0300

    setup and sample test for TestController

---
+++ aspnetcore-app-example/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore_app_example.Controllers
+{
+    [Route("api/test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("TestResult");
+        }
+    }
+}
